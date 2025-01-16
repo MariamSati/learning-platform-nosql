5b94c6b5e5c6c8ae191c547a0eefb3d5be2dbfd2
MariamSati <mariamsati@gmail.com>
2025-01-16 12:30:58 +0100

    connexion a mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 // Question : Pourquoi créer un module séparé pour les connexions aux bases de données ?
-// Réponse : 
+// Réponse :  Pour centraliser la gestion des connexions, faciliter la maintenance, réutiliser le code et garantir une meilleure gestion des erreurs.
 // Question : Comment gérer proprement la fermeture des connexions ?
-// Réponse : 
+// Réponse : Pour gérer proprement la fermeture des connexions, utilisez des gestionnaires d'événements ou des hooks (comme process.on('SIGINT')) pour fermer les connexions à la base de données et libérer les ressources avant que l'application ne se termine.
 
 const { MongoClient } = require('mongodb');
 const redis = require('redis');
@@ -12,6 +12,18 @@ let mongoClient, redisClient, db;
 async function connectMongo() {
   // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  try {
+    mongoClient = new MongoClient(config.mongodb.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await mongoClient.connect();
+    db = mongoClient.db(config.mongodb.dbName);
+    console.log('MongoDB connected successfully!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit on failure
+  }
 }
 
 async function connectRedis() {
